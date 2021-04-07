44d2fba876707f97a8172e493a9911d2e9a322f1
emihhorn <emilyhopehorn@gmail.com>
2021-04-07 12:01:31 -0500

    hello luna

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,9 +1,25 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PipedWriter;
+import java.io.PrintWriter;
 
+@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello-world")
 public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
 
+    private int hitCount;
+
+    //reset hitCount
+
+    public void init() {
+        hitCount = 0;
+    }
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        PrintWriter out = res.getWriter();
+        out.println("<h1>Hello Luna!");
     }
 }
