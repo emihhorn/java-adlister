acc76ed78a341188f3b22bbf93406517e1da0b19
emihhorn <emilyhopehorn@gmail.com>
2021-04-15 12:03:45 -0500

    sessions-cookies

--- src/main/java/AdsIndexServlet.java
+++ src/main/java/AdsIndexServlet.java
@@ -1,4 +1,5 @@
 import javax.servlet.ServletException;
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -10,6 +11,6 @@ import java.util.List;
 public class AdsIndexServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.setAttribute("ads", DaoFactory.getAdsDao().all());
-        request.getRequestDispatcher("/ads/index.jsp").forward(request, response);
+        request.getRequestDispatcher("/WEB-INF/ads/index.jsp").forward(request, response);
     }
 }
